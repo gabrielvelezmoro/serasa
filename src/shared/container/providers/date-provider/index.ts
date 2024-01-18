b4f0d4050825cc0afb7490f25f9ed0db7f14f574
gabriel <gabrieuvelezmoro@gmail.com>
2024-01-18 17:59:24 -0300

    refactor: remove ununecessary code

--- src/shared/container/providers/date-provider/index.ts
+++
@@ -1,8 +0,0 @@
-import { container } from "tsyringe";
-import { IDateProvider } from "./i-date-provider";
-import { DayjsDateProvider } from "./implementations/day-js-date-provider";
-
-container.registerSingleton<IDateProvider>(
-  "DayjsDateProvider",
-  DayjsDateProvider
-);
